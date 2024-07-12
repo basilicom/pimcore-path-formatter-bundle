84fc44973258f9be8db32f2ce0477c67ad32024d
alexander.heidrich <alexander.heidrich@basilicom.de>
2024-07-12 08:45:25 +0200

    feature: option to enable/disable inheritance of dataobject values

--- src/DependencyInjection/ConfigDefinition.php
+++ src/DependencyInjection/ConfigDefinition.php
@@ -15,7 +15,7 @@ class ConfigDefinition implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('basilicom_path_formatter');
         $treeBuilder
